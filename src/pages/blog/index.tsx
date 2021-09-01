8b6ff640d79c2f068cdacfa3dd7c949728225d1d
Steven Testa <steve@testasoftware.com>
2021-09-01 15:44:56 -0400

    minor blog index refactor

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -5,31 +5,28 @@ import DefaultLayout from "../../layouts/default"
 import Seo from "../../components/site/seo"
 
 const PostSummary = ({ frontmatter, excerpt }) => (
-  <div className="col-lg-8 mx-auto text-center">
-    <div className="card card-blog card-plain">
-      <div className="position-relative">
-        <Link className="d-block blur-shadow-image" to={"/blog/" + frontmatter.slug}>
-          <img src={frontmatter.image} alt="img-blur-shadow" className="img-fluid shadow border-radius-lg w-100" />
-        </Link>
-      </div>
-      <div className="card-body px-0 pt-4">
-        {frontmatter.categories.map(cat => (
-        <span className="text-gradient text-primary text-gradient font-weight-bold text-sm text-uppercase">{cat} </span>
-        ))}
-        <Link to={"/blog/" + frontmatter.slug}>
-          <h4>
-            {frontmatter.title}
-          </h4>
-        </Link>
-        <p>
-          {excerpt}
-        </p>
-        <Link to={"/blog/" + frontmatter.slug}>
-          <button type="button" className="btn bg-gradient-primary mt-3">Read more</button>
-        </Link>
-      </div>
+  <div className="card card-blog card-plain">
+    <div className="position-relative">
+      <Link className="d-block blur-shadow-image" to={"/blog/" + frontmatter.slug}>
+        <img src={frontmatter.image} alt="img-blur-shadow" className="img-fluid shadow border-radius-lg w-100" />
+      </Link>
     </div>
-    <hr className="horizontal dark my-6" />
+    <div className="card-body px-0 pt-4">
+      {frontmatter.categories.map(cat => (
+      <span className="text-gradient text-primary text-gradient font-weight-bold text-sm text-uppercase">{cat} </span>
+      ))}
+      <Link to={"/blog/" + frontmatter.slug}>
+        <h4>
+          {frontmatter.title}
+        </h4>
+      </Link>
+      <p>
+        {excerpt}
+      </p>
+      <Link to={"/blog/" + frontmatter.slug}>
+        <button type="button" className="btn bg-gradient-primary mt-3">Read more</button>
+      </Link>
+    </div>
   </div>
 )
 
@@ -41,9 +38,11 @@ const BlogPage = ({ data }) => {
       <section className="pt-10">
         <div className="container">
           <div className="row">
+            <div className="col-lg-8 mx-auto text-center">
             {posts.map(post => (
               <PostSummary frontmatter={post.node.frontmatter} excerpt={post.node.excerpt} />
             ))}
+            </div>
           </div>
         </div>
       </section>
